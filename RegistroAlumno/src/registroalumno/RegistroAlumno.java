b84e2285762e13d1d0ab218bf60e0cb9e0f792b9
Maximiliano Castro <maxicastro628@gmail.com>
2025-09-02 20:26:46 -0300

    Agregue en los objetos alumnos nuestros nombres y que muestre la cantidad materias de cada uno

--- RegistroAlumno/src/registroalumno/RegistroAlumno.java
+++ RegistroAlumno/src/registroalumno/RegistroAlumno.java
@@ -16,8 +16,13 @@ public class RegistroAlumno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Alumno a = new Alumno (10, "Lopez", "Martin");
-        Alumno a1 = new Alumno (10, "Martinez", "Brenda");
+        Alumno a = new Alumno (10, "Castro", "Maximiliano");
+        Alumno a1 = new Alumno (10, "Kjellfrid", "Bjerkelli");
+        Alumno a2 = new Alumno (10, "Fernandez", "Rodrigo");
+        Alumno a3 = new Alumno (10, "Barros", "Edgardo");
+        Alumno a4 = new Alumno (10, "Carrizo", "Anna");
+        Alumno a5 = new Alumno (10, "Domene", "Cora");
+        Alumno a6 = new Alumno (10, "Gimenez", "Pablo");
         Materia m1 = new Materia(1, "Matematica 1", 1);
         Materia m2 = new Materia (2, "Laboratorio 1", 1);
         Materia m3= new Materia (3, "Web 2", 1);
@@ -28,10 +33,35 @@ public class RegistroAlumno {
         a1.agregarMaterias(m1);
         a1.agregarMaterias(m2);
         a1.agregarMaterias(m3);
-        a1.agregarMaterias(m3);
+        a1.mostrar();
+        a2.agregarMaterias(m1);
+        a2.agregarMaterias(m2);
+        a2.agregarMaterias(m3);
+        a2.mostrar();
+        a3.agregarMaterias(m1);
+        a3.agregarMaterias(m2);
+        a3.agregarMaterias(m3);
+        a3.mostrar();
+        a4.agregarMaterias(m1);
+        a4.agregarMaterias(m2);
+        a4.agregarMaterias(m3);
+        a4.mostrar();
+        a5.agregarMaterias(m1);
+        a5.agregarMaterias(m2);
+        a5.agregarMaterias(m3);
+        a5.mostrar();
+        a6.agregarMaterias(m1);
+        a6.agregarMaterias(m2);
+        a6.agregarMaterias(m3);
+        a6.mostrar();
         
         a1.mostrar();
         System.out.println("La cantidad de materias que cursa el alumno "+a.getApellido()+" es: " +a.catidadMateria());
         System.out.println("La cantidad de materias que cursa el alumno "+a1.getApellido()+" es: " +a1.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a2.getApellido()+" es: " +a2.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a3.getApellido()+" es: " +a3.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a4.getApellido()+" es: " +a4.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a5.getApellido()+" es: " +a5.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a6.getApellido()+" es: " +a6.catidadMateria());
     }
 }
