a4d623806c713abd9c56d60ed5b36386cf7148d4
Maximiliano Castro <maxicastro628@gmail.com>
2025-09-02 20:16:13 -0300

    Cambie algunas cossas y agregue al segundo alumno

--- RegistroAlumno/src/registroalumno/RegistroAlumno.java
+++ RegistroAlumno/src/registroalumno/RegistroAlumno.java
@@ -16,15 +16,22 @@ public class RegistroAlumno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Alumno a = new Alumno (10, "Barros", "Edgardo");
+        Alumno a = new Alumno (10, "Lopez", "Martin");
+        Alumno a1 = new Alumno (10, "Martinez", "Brenda");
         Materia m1 = new Materia(1, "Matematica 1", 1);
-        Materia m2 = new Materia (2, "Programacion", 1);
-        Materia m3= new Materia (3, "web", 1);
+        Materia m2 = new Materia (2, "Laboratorio 1", 1);
+        Materia m3= new Materia (3, "Web 2", 1);
         a.agregarMaterias(m1);
         a.agregarMaterias(m2);
         a.agregarMaterias(m3);
         a.mostrar();
-        System.out.println("La cantidad de materias que cursa es: " +a.catidadMateria());
+        a1.agregarMaterias(m1);
+        a1.agregarMaterias(m2);
+        a1.agregarMaterias(m3);
+        a1.agregarMaterias(m3);
+        
+        a1.mostrar();
+        System.out.println("La cantidad de materias que cursa el alumno "+a.getApellido()+" es: " +a.catidadMateria());
+        System.out.println("La cantidad de materias que cursa el alumno "+a1.getApellido()+" es: " +a1.catidadMateria());
     }
-    
 }
